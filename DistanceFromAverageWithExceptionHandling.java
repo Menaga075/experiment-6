d131d3a562f3b2da2baf89480c7bacca1c1d9896
Menaga075 <111369247+Menaga075@users.noreply.github.com>
2022-09-28 12:48:02 +0530

    Update DistanceFromAverageWithExceptionHandling.java

--- DistanceFromAverageWithExceptionHandling.java
+++ DistanceFromAverageWithExceptionHandling.java
@@ -1,16 +1,4 @@
-
 import java.util.Scanner;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author MENAGA
- * 
- */
 public class DistanceFromAverageWithExceptionHandling 
 {
      public static void main (String[] args)
